339b48005b1b1fc491189b76a308d969297ae695
Jovani Arzate Cabrera <jovaniac@gmail.com>
2018-07-16 00:34:36 -0500

    orquestacion de negocio

--- src/main/java/com/hazloakki/negocio/NegocioApplication.java
+++ src/main/java/com/hazloakki/negocio/NegocioApplication.java
@@ -2,7 +2,9 @@ package com.hazloakki.negocio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NegocioApplication {
 
